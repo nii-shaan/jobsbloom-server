1980a67d2cf8d6440b47f9a0594e3f13fa758894
Nishan Bista <nishanbista525@gmail.com>
2024-10-22 00:34:04 +0545

    setup

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express()
+
+app.use(
+  cors({
+    origin: process.env.REQUEST_ORIGIN,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+  })
+)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(cookieParser());
+
+app.get("/", (_req, res) => {
+  res.json("if you dont know why you're here, please close the tab")
+})
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("this is index file");
+import dotenv from "dotenv"
+dotenv.config()
+
+import app from "./app.js"
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(8000, () => {
+  console.log(`SERVER RUNNING ON PORT: ${PORT}`)
+})
+
